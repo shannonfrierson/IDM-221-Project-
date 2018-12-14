b9452caa3e8931d5f7028d859e8b961ef90f742b
shannonfrierson <43862525+shannonfrierson@users.noreply.github.com>
2018-12-14 11:49:54 -0500

    Final HTML, CSS, and JS Pages

---
+++ master.js
@@ -0,0 +1,9 @@
+
+function showhide() {
+    var div = document.getElementById ("links");
+    if (div.style.display == "none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
